cddab0b7610617256bf2780209a3a8f6db78af8e
etna patricia davis osorio <14-10281@usb.ve>
2020-03-07 22:00:52 -0400

    paso 8

--- script.py
+++ script.py
@@ -4,3 +4,13 @@ from pprint import pprint
 response = requests.post('https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token', auth=('devnetuser','Cisco123!'))
 payload=response.json()
 pprint(payload)
+
+
+
+
+
+
+
+
+
+response.raise_for_status(),
